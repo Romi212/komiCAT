a0d6201539e9e06c4396d1bc0657d15f96e60867
Romina Garcia <104693764+Romi212@users.noreply.github.com>
2026-05-29 17:16:12 -0300

    creating page class

---
+++ src/aux_types/page.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Page:
+    def __init__(self, file_path=None, image=None):
+        
+        self.text_boxes = []
+        self.file_path = file_path
+        self.image = image
+        self.page_name = os.path.basename(file_path)
